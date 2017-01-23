1e2f6d5f8778a342b1d33151bcf4c88351885b93
mventura <mventura@savvysherpa.com>
2017-01-23 14:59:20 +0800

    add function for zero-crossings count

--- notebooks/sleep_utils.py
+++ notebooks/sleep_utils.py
@@ -9,6 +9,7 @@ import seaborn as sns
 from scipy.ndimage import convolve1d
 import itertools
 from peakdetect import peakdetect
+from imblearn.over_sampling import SMOTE
 
 
 def getwindow(signal, window_duration = 16, window_interval=1, f_s = 4):
@@ -62,7 +63,7 @@ def heart_rate_var(time_peaks):
 
 def bio_signal_peak_detect(sig, fs, sigtype = 'resp'):
     signaltype = {'resp': fs/2*4, 'ecg': fs/2*2, 'bp': fs/2}
-    max_peaks, min_peaks = peakdetect(sig, lookahead = signaltype[sigtype])
+    max_peaks, min_peaks = peakdetect(sig, lookahead = int(signaltype[sigtype]))
     max_peaks_idx, max_peaks_val = zip(*max_peaks)
     min_peaks_idx, min_peaks_val = zip(*min_peaks)
     
@@ -73,10 +74,52 @@ def compute_amplitude(sig, fs, sigtype):
     if max_peaks_idx[0] > min_peaks_idx[0]:
         max_peaks_idx = max_peaks_idx[1:]
         max_peaks_val = max_peaks_val[1:]
+
     if len(max_peaks_idx) > len(min_peaks_idx):
-        amplitude = np.array(max_peaks_val[-len(min_peaks_idx):]) - np.array(min_peaks_val[-len(min_peaks_idx):])
+        if (len(max_peaks_val[-len(min_peaks_idx):]) == 0) or (len(min_peaks_val[-len(min_peaks_idx):]) == 0):
+            amplitude = [np.nan]
+        else:
+            amplitude = np.array(max_peaks_val[-len(min_peaks_idx):]) - np.array(min_peaks_val[-len(min_peaks_idx):])
     elif len(max_peaks_idx) < len(min_peaks_idx):
-        amplitude = np.array(max_peaks_val[-len(max_peaks_idx):]) - np.array(min_peaks_val[-len(max_peaks_idx):])
+        if (len(max_peaks_val[-len(max_peaks_idx):])) == 0 or (len(min_peaks_val[-len(max_peaks_idx):]) == 0):
+            amplitude = [np.nan]
+        else:
+            amplitude = np.array(max_peaks_val[-len(max_peaks_idx):]) - np.array(min_peaks_val[-len(max_peaks_idx):])
     else: 
-        amplitude = np.array(max_peaks_val) - np.array(min_peaks_val)
+        if (len(max_peaks_val) == 0) or (len(min_peaks_val) == 0):
+            amplitude = [np.nan]
+        else:
+            amplitude = np.array(max_peaks_val) - np.array(min_peaks_val)
     return amplitude
+
+def Zero_Crossing_Counts(arr):
+    """
+    Returns the number/counts of zero crossings
+    """
+    return len(np.where(np.diff(np.sign(arr-np.mean(arr))))[0])
+
+def smote_all_minority(train_set, train_label):
+    sm = SMOTE(kind='regular')
+
+    majority = max(set(train_label), key=list(train_label).count)
+    major_index = np.where(train_label==majority)[0]
+    minorities = list(set(train_label) - set([majority]))
+    
+    majority_data = train_set[major_index, :]
+    majority_labels = train_label[major_index]
+    
+    oversampled_data = {majority: majority_data}
+    oversampled_labels = {majority: majority_labels}
+    
+    for i in minorities:
+        minor_index = np.where(train_label==i)[0]
+        reduced_features = np.concatenate((train_set[major_index, :], train_set[minor_index, :]))
+        reduced_labels = np.concatenate((train_label[major_index], train_label[minor_index]))  
+        train_set_smote, train_label_smote = sm.fit_sample(reduced_features, reduced_labels)
+        
+        new_minor_index = np.where(train_label_smote==i)[0]
+
+        oversampled_data[i] = train_set_smote[new_minor_index, :]
+        oversampled_labels[i] = train_label_smote[new_minor_index]
+        
+    return oversampled_data, oversampled_labels
